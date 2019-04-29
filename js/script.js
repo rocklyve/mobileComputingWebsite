49f422a723773d9d0abcc967a35d4fe5d7527140
David Laubenstein <david.laubenstein@posteo.de>
2019-04-29 10:46:37 +0200

    revert

--- js/script.js
+++ js/script.js
@@ -1,6 +1,3 @@
-var roseTmp = window.getElementById("rose")
-roseTmp.style.transform = "rotate(" + event.alpha + "deg)"
-
 window.addEventListener("deviceorientation", function(event) {
 	var rose = document.querySelector("#rose")
 	rose.style.transform = "rotate(" + event.alpha + "deg)"
