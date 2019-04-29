75501ff480d83780b3ef55ad8f6af1a197f0989c
David Laubenstein <david.laubenstein@posteo.de>
2019-04-29 10:44:03 +0200

    Rotation should now be set once on startup

--- js/script.js
+++ js/script.js
@@ -1,17 +1,7 @@
-window.ondevicemotion = function(event) { 
-	var ax = event.accelerationIncludingGravity.x
-	var ay = event.accelerationIncludingGravity.y
-	var az = event.accelerationIncludingGravity.z
-
-	document.querySelector("#x_acc").innerHTML = "X = " + ax;
-	document.querySelector("#y_acc").innerHTML = "Y = " + ay;
-	document.querySelector("#z_acc").innerHTML = "Z = " + az;
-}
+var roseTmp = window.getElementById("rose")
+roseTmp.style.transform = "rotate(" + event.alpha + "deg)"
 
 window.addEventListener("deviceorientation", function(event) {
 	var rose = document.querySelector("#rose")
 	rose.style.transform = "rotate(" + event.alpha + "deg)"
-	
-	document.querySelector("#mag_beta").innerHTML = "beta = " + event.beta;
-	document.querySelector("#mag_gamma").innerHTML = "gamma = " + event.gamma;
 }, true);
